badc710462604201683a55268673be7e2e8a0372
dev-rfgul <rfgul587@gmail.com>
2025-06-13 16:49:54 +0500

    almost finalized , till here the msgs are being sent, response is displayed, history is also shown

--- backend/controller/patternMatch.js
+++ backend/controller/patternMatch.js
@@ -1,14 +1,15 @@
 import patterns from '../pattern.js';
 
 export const pattern = async (req,res) => {
-    const userQuery = req.body.message;
-    const matchPattern = patterns.find(item => item.pattern.test(userQuery));
+    const userQuery = req.body;
+    console.log("line no 5",userQuery)
+    const matchPattern = patterns.find(item => item.pattern.test(userQuery.query));
     if (matchPattern) {
         res.json({ answer: matchPattern.answer });
     } else {
         res.json({ answer: "Sorry, I didn't understand that. Can you please rephrase?" });
     }
-    console.log(`User Query: ${userQuery}`);
+    console.log(`User Query: ${userQuery.query}`);
     console.log(`Matched Answer: ${matchPattern ? matchPattern.answer : "No match found"}`);
 }
 export default pattern;
